70d83467572f9bf126bb520cc4d09d8d08811477
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-05-30 20:42:15 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,11 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('/todo' , \App\Http\Controllers\TodolistController::class);
+
+// Route::artisan call
+Route::get('/artisan-call', function () {
+    Artisan::call('storage:link'); // storage:link
+    Artisan::call('route:clear'); // route:clear
+    Artisan::call('config:clear'); // config:clear
+    return 'success';
+});
